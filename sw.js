b81a85c7295b23a7323f6430523a8e7c4d3fd4d5
chq576 <3320641422@qq.com>
2025-10-18 22:53:41 +0800

    Site updated: 2025-10-18 22:53:41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760799008496";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760799207931";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
