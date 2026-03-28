2ef6fdde9f3d548f3e5c47acff530e64037715f4
chq576 <3320641422@qq.com>
2026-03-28 11:43:30 +0800

    Site updated: 2026-03-28 11:43:29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1774669165649";const preCache = ["/images/taichi.png","/images/cc.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1774669407083";const preCache = ["/images/taichi.png","/images/cc.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
